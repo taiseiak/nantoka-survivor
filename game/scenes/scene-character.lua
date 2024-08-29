9ca79b13602a6e36d1ba66cddebe25eb4e19054f
Taito <3835littletaitomonster@gmail.com>
2024-08-29 23:50:28 +0900

    なんとか矢印キーで動くようにしたかったがbootluaエラーが出て動かない

--- game/scenes/scene-character.lua
+++ game/scenes/scene-character.lua
@@ -13,20 +13,40 @@ function game:load(args)
   })
   self.text:send("Hello World!", 200)
 
+  -- 画像の読み込み
   self.image = love.graphics.newImage(
-    "/Users/miyasakotaito/Desktop/nantoka-survivor/game/assets/sprites/playdate_circle.png")
+    "game/assets/sprites/playdate_circle.png")
+  -- 画像の初期位置
+  self.imageX = gameMidX
+  self.imageY = gameMidY
+  -- 画像の移動速度
+  self.speed = 200
 end
 
 function game:update(dt)
   self.text:update(dt)
+
+  -- 矢印キーでの移動
+  if love.keyboard.isDown("left") then
+    self.imageX = self.imageX - self.speed * dt
+  end
+  if love.keyboard.isDown("right") then
+    self.imageX = self.imageX + self.speed * dt
+  end
+  if love.keyboard.isDown("up") then
+    self.imageY = self.imageY - self.speed * dt
+  end
+  if love.keyboard.isDown("down") then
+    self.imageY = self.imageY + self.speed * dt
+  end
 end
 
 function game:draw()
   self.text:draw(gameMidX - self.text.get.width / 2, gameMidY - self.text.get.height / 2)
 
-  local imagewidth = self.image:getWidth()
+  local imageWidth = self.image:getWidth()
   local imageHeight = self.image:getHeight()
-  love.graphics.draw(self.image, gameMidX - imagewidth / 2, gameMidY - imageHeight / 2)
+  love.graphics.draw(self.image, self.imageX - imageWidth / 2, self.imageY - imageHeight / 2)
 end
 
 return game
