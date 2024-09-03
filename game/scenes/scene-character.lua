f251efee367ce526dd833f413d3143f3518376ce
Taito <3835littletaitomonster@gmail.com>
2024-09-03 18:56:11 +0900

    変更しました

--- game/scenes/scene-character.lua
+++ game/scenes/scene-character.lua
@@ -1,11 +1,23 @@
 local Text = require("libraries.slog-text")
 local Push = require("libraries.push")
+local Baton = require("libraries.baton")
 
 local game = {}
 
 local gameMidX = G.gameWidth / 2
 local gameMidY = G.gameHeight / 2
 
+local input = Baton.new {
+  controls = {
+    left = { 'key:left' },
+    right = { 'key:right' },
+    up = { 'key:up' },
+    down = { 'key:down' },
+    -- action = {'key:x', 'button:a'},
+  },
+  pairs = {
+    move = { 'left', 'right', 'up', 'down' } },
+}
 function game:load(args)
   -- self.text = Text.new("center", {
   --   color = G.palette[2],
@@ -27,18 +39,26 @@ function game:update(dt)
   -- self.text:update(dt)
 
   -- 矢印キーでの移動
-  if love.keyboard.isDown("left") then
-    self.imageX = self.imageX - self.speed * dt
-  end
-  if love.keyboard.isDown("right") then
-    self.imageX = self.imageX + self.speed * dt
-  end
-  if love.keyboard.isDown("up") then
-    self.imageY = self.imageY - self.speed * dt
-  end
-  if love.keyboard.isDown("down") then
-    self.imageY = self.imageY + self.speed * dt
-  end
+  --   if love.keyboard.isDown("left") then
+  --     self.imageX = self.imageX - self.speed * dt
+  --   end
+  --   if love.keyboard.isDown("right") then
+  --     self.imageX = self.imageX + self.speed * dt
+  --   end
+  --   if love.keyboard.isDown("up") then
+  --     self.imageY = self.imageY - self.speed * dt
+  --   end
+  --   if love.keyboard.isDown("down") then
+  --     self.imageY = self.imageY + self.speed * dt
+  --   end
+  input:update()
+
+  -- 入力で方向を取得
+  local x, y = input:get "move"
+
+  -- 画像をその方向に移動
+  self.imageX = self.imageX + x * self.speed * dt
+  self.imageY = self.imageY + y * self.speed * dt
 end
 
 function game:draw()
