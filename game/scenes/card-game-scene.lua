9e73401d135d5eb4fe962111a9195ca1cc736958
T <taiseiklasen@gmail.com>
2024-09-03 17:22:27 +0900

    not working cards

--- game/scenes/card-game-scene.lua
+++ game/scenes/card-game-scene.lua
@@ -1,6 +1,5 @@
-local Text = require("libraries.slog-text")
-local Push = require("libraries.push")
 local Vector = require("libraries.brinevector")
+local Baton = require("libraries.baton")
 
 local game = {}
 
@@ -8,28 +7,43 @@ local gameMidX = G.gameWidth / 2
 local gameMidY = G.gameHeight / 2
 
 function game:load(args)
+    -- Input handling
+    self.input = Baton.new(
+        {
+            controls = {
+                cardUp = { "key:up" },
+                cardDown = { "key:down" },
+                useCard = { "key:left" }
+            }
+        }
+    )
+
     local cardImage = love.graphics.newImage("assets/sprites/nantoka-survivor-card.png")
+
+    self.cardPositions = {
+        Vector(G.gameWidth - cardImage:getWidth() / 2 - 5, gameMidY - cardImage:getHeight() / 2 - 60),
+        Vector(G.gameWidth - cardImage:getWidth() / 2 - 18, gameMidY - cardImage:getHeight() / 2 - 35),
+        Vector(G.gameWidth - cardImage:getWidth() / 2 - 35, gameMidY - cardImage:getHeight() / 2),
+        Vector(G.gameWidth - cardImage:getWidth() / 2 - 18, gameMidY - cardImage:getHeight() / 2 + 35),
+        Vector(G.gameWidth - cardImage:getWidth() / 2 - 5, gameMidY - cardImage:getHeight() / 2 + 60),
+    }
+
     cardImage:setFilter("nearest", "nearest")
     self.cards = {}
     table.insert(self.cards, {
         sprite = cardImage,
-        vector = Vector(G.gameWidth - cardImage:getWidth() / 2 - 5, gameMidY - cardImage:getHeight() / 2 - 60)
     })
     table.insert(self.cards, {
         sprite = cardImage,
-        vector = Vector(G.gameWidth - cardImage:getWidth() / 2 - 5, gameMidY - cardImage:getHeight() / 2 + 60)
     })
     table.insert(self.cards, {
         sprite = cardImage,
-        vector = Vector(G.gameWidth - cardImage:getWidth() / 2 - 18, gameMidY - cardImage:getHeight() / 2 + 35)
     })
     table.insert(self.cards, {
         sprite = cardImage,
-        vector = Vector(G.gameWidth - cardImage:getWidth() / 2 - 18, gameMidY - cardImage:getHeight() / 2 - 35)
     })
     table.insert(self.cards, {
         sprite = cardImage,
-        vector = Vector(G.gameWidth - cardImage:getWidth() / 2 - 35, gameMidY - cardImage:getHeight() / 2)
     })
 
     self.circle = love.graphics.newImage("assets/sprites/playdate_circle.png")
@@ -39,6 +53,11 @@ function game:load(args)
 end
 
 function game:update(dt)
+    self.input:update()
+
+    if self.input:pressed("cardUp") then
+
+    end
 end
 
 function game:draw()
